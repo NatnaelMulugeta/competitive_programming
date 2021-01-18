f7dce3ca04d34c6d5a353fde66a3bf1899ebdef2
Natnael Mulugeta <natnaelmu.07@gmail.com>
2021-01-18 11:35:32 +0300

    sort colors

--- leetcode/sort colors.py
+++ leetcode/sort colors.py
@@ -15,22 +15,6 @@ def sortColors(nums):
             nums[b] = 2
             b -= 1
     print(nums)
-    '''
-
-    n = len(nums)
-    for i in range(0, n):
-        if nums[i] == 0:
-            nums[w] = nums[r]
-            nums[r] = 0
-            r += 1
-            w += 1
-        elif nums[i] == 1:
-            w += 1
-        elif nums[i] == 2:
-            nums[w] = nums[b]
-            nums[b] = 2
-            b -= 1
-    print(nums)
 
 
 

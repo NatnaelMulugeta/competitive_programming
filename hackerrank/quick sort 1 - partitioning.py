74db62f5a498910bcb6da59828c865143c93f1be
Natnael Mulugeta <natnaelmu.07@gmail.com>
2021-01-20 10:46:26 +0300

    quick sort

---
+++ hackerrank/quick sort 1 - partitioning.py
@@ -0,0 +1,16 @@
+def quickSort(arr):
+    if len(arr) < 2:
+        return arr
+    left, equal, right = [], [], []
+    equal.append(arr[0])
+    for i in range(1, len(arr)):
+        if arr[i] < arr[0]:
+            left.append(arr[i])
+        elif arr[i] > arr[0]:
+            right.append(arr[i])
+        else:
+            equal.append(arr[i])
+
+    return left + equal + right
+
+print(quickSort([4,5,3,7,2]))

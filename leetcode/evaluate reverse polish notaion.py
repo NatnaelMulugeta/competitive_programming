0ccf15affb981dcc80a7f98177a1da65db3bcea0
Natnael Mulugeta <natnaelmu.07@gmail.com>
2021-01-21 18:03:19 +0300

    evaluate reverse polish notaion

--- leetcode/evaluate reverse polish notaion.py
+++ leetcode/evaluate reverse polish notaion.py
@@ -1,5 +1,5 @@
 class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
+    def evalRPN(self, tokens: [str]):
         stack = []
         op = ['+', '-', '*', '/']
         for token in tokens:

c39d4dd7ca206c2d6a5a2876f279a9cd9e5a7528
Natnael Mulugeta <natnaelmu.07@gmail.com>
2021-01-21 19:26:06 +0300

    daily tempratures

---
+++ leetcode/daily tempratures.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, T: [int]) -> [int]:
+        stack = []
+        result = [0] * len(T)
+        for i in range(len(T)-1, -1, -1):
+            while stack and T[i] >= T[stack[-1]]:
+                stack.pop()
+            if stack:
+                result[i] = stack[-1] - i
+            stack.append(i)
+
+        return result
+

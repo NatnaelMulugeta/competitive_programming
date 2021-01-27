2f7352d3658d44798c109c07efcb5047e318af0e
Natnael Mulugeta <natnaelmu.07@gmail.com>
2021-01-27 13:31:35 +0300

    sort array by increasing frequency

--- leetcode/sort array by increasing frequency.py
+++ leetcode/sort array by increasing frequency.py
@@ -1,7 +1,7 @@
 from collections import Counter
 class Solution:
     def frequencySort(self, nums: [int]) -> [int]:
-        count = dict(Counter(nums))
+        count = Counter(nums)
         result = []
         while count:
             key = nums[0]
@@ -18,6 +18,7 @@ class Solution:
             del count[key]
         
         return result
+        
         
 obj = Solution()
 print(obj.frequencySort([1,2,1,1,0,2,3]))

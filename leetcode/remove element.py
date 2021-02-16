959d249a31c5224cde721cc96d4c89e67a945b4d
Natnael Mulugeta <natnaelmu.07@gmail.com>
2021-02-17 00:39:02 +0300

    remove element

--- leetcode/remove element.py
+++ leetcode/remove element.py
@@ -1,5 +1,5 @@
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
+    def removeElement(self, nums: [int], val: int) -> int:
         for num in nums[:]:
             if num == val:
                 nums.remove(val)

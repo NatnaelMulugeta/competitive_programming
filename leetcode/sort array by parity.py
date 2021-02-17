d8a9a968749772ce0e4aabc2a22c74016975185b
Natnael Mulugeta <natnaelmu.07@gmail.com>
2021-02-17 12:15:47 +0300

    sort array by parity

--- leetcode/sort array by parity.py
+++ leetcode/sort array by parity.py
@@ -1,6 +1,6 @@
 
 class Solution:
-    def sortArrayByParity(self, A: List[int]) -> List[int]:
+    def sortArrayByParity(self, A: [int]) -> [int]:
         even, odd = [], []
         for num in A:
             odd.append(num) if num % 2 else even.append(num)

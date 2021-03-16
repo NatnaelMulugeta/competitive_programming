1362d96fbfc590292129248ec513915d947a64d2
Natnael Mulugeta <natnaelmu.07@gmail.com>
2021-03-16 22:31:24 +0300

    maximum nesting of the parentheses

--- leetcode/maximum nesting depth of the parentheses.py
+++ leetcode/maximum nesting depth of the parentheses.py
@@ -1,13 +1,13 @@
-def maxDepth(s):
-    depth = 0
-    par = 0
-    for i in s:
-        if i == '(':
-            par += 1
-            depth = max(depth, par)
-        if i == ')':
-            par -= 1
-            depth = max(depth, par)
-    return depth
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        result, curr = 0, 0
+        
+        for c in s:
+            if c == "(": curr += 1
+            elif c == ")": curr -= 1
+                
+            result = max(result, curr)
+            
+        return result
 
 print(maxDepth("(1+(2*3)+((8)/4))+1"))

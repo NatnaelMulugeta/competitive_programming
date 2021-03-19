2c493f56314964b0ae19e5564c89c4adf1344885
Natnael Mulugeta <natnaelmu.07@gmail.com>
2021-03-19 11:27:38 +0300

    binary tree level order traversal

--- leetcode/binary tree level order traversal.py
+++ leetcode/binary tree level order traversal.py
@@ -1,27 +1,60 @@
+from collections import deque
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-        
+
 class Solution:
     def levelOrder(self, root: TreeNode) -> [[int]]:
-        result = []
-        if not root:
-            return result
         
-        curr = [root]
-        while curr:
-            result.append([node.val for node in curr])
+        # BFS
+        
+        if not root: return
+        
+        result = []
+        
+        queue = deque([(root, 0)])
+        
+        while queue:
+            node, level = queue.popleft()
             
-            next_level = []
-            for node in curr:
-                if node.left:
-                    next_level.append(node.left)
-                if node.right:
-                    next_level.append(node.right)
-                    
-            curr = next_level
-            
+            if len(result) > level:
+                result[level].append(node.val)
+            else:
+                result.append([node.val])
+                          
+            if node.left:
+                queue.append((node.left, level + 1))
+                
+            if node.right:
+                queue.append((node.right, level + 1))
+                
         return result
+    
+    
+        # iterative
+    
+#         if not root: return result
+        
+#         result = []
+        
+        
+#         curr = [root]
+#         while curr:
+#             result.append([node.val for node in curr])
+            
+#             next_level = []
+#             for node in curr:
+                
+#                 if node.left:
+#                     next_level.append(node.left)
+                
+#                 if node.right:
+#                     next_level.append(node.right)
+                    
+#             curr = next_level
+            
+#         return result
--- leetcode/binary tree postorder traversal.py
+++ leetcode/binary tree postorder traversal.py
@@ -1,20 +1,48 @@
 # Definition for a binary tree node.
 class TreeNode:
-     def __init__(self, val=0, left=None, right=None):
-         self.val = val
-         self.left = left
-         self.right = right
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def postorderTraversal(self, root: TreeNode) -> [int]:
+        
+        # iterative
+        if not root: return
+        
         result = []
-        if root == None:
-            return result
-        node = root
-        if node.left:
-            result += self.postorderTraversal(node.left)
-        if node.right:
-            result += self.postorderTraversal(node.right)
-        result.append(node.val)
-        return result
+        
+        stack = []
+        while stack or root:
+            
+            if root:
+                result.append(root.val)
+                stack.append(root)
+                root = root.right
+            
+            else:
+                root = stack.pop().left
+                
+        return result[::-1]
+
+        
+        # recursive
+        
+#         result = []
+        
+#         if root == None: return result
+        
+#         node = root
+        
+#         if node.left:
+#             result += self.postorderTraversal(node.left)
+        
+#         if node.right:
+#             result += self.postorderTraversal(node.right)
+        
+#         result.append(node.val)
+        
+#         return result
     
     

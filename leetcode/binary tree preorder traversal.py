d5ffb93e873c65a6db109b1b677efa871ff0b017
Natnael Mulugeta <natnaelmu.07@gmail.com>
2021-03-19 11:20:13 +0300

    binary tree preorder traversal

--- leetcode/binary tree preorder traversal.py
+++ leetcode/binary tree preorder traversal.py
@@ -1,36 +1,46 @@
 # Definition for a binary tree node.
 class TreeNode:
-     def __init__(self, val=0, left=None, right=None):
-         self.val = val
-         self.left = left
-         self.right = right
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def preorderTraversal(self, root: TreeNode) -> [int]:
-        if not root:
-            return
+        
+        # recursive
+        
+#         if not root: return
+        
+#         result = []
+#         self.helper(root, result)
+        
+#         return result
+    
+#     def helper(self, node: TreeNode, lst: List[int]):
+#         if not node: return
+        
+#         lst.append(node.val)
+#         self.helper(node.left, lst)
+#         self.helper(node.right, lst)
+
+        
+        # iterative
+        
+        if not root: return
+        
         result = []
         stack = [root]
+        
         while stack:
             node = stack.pop()
+            
             result.append(node.val)
+            
             if node.right:
                 stack.append(node.right)
             if node.left:
                 stack.append(node.left)
                 
         return result
-           
-        # recursive function
-        
-class SolutionTwo:    
-    def preorderTraversal(self, root: TreeNode) -> [int]:
-        lst = []
-        self.helper(root, lst)
-        return lst
-    def helper(self, root: TreeNode, lst: [int]):
-        if not root:
-            return
-        lst.append(root.val)
-        self.helper(root.left, lst)
-        self.helper(root.right, lst)       
         

9be379e43c445b8d3d816f56afc8fce00a12e897
Natnael Mulugeta <natnaelmu.07@gmail.com>
2021-03-19 11:21:41 +0300

    binary tree inorder traversal

--- leetcode/binary tree inorder traversal.py
+++ leetcode/binary tree inorder traversal.py
@@ -1,17 +1,47 @@
 # Definition for a binary tree node.
 class TreeNode:
-     def __init__(self, val=0, left=None, right=None):
-         self.val = val
-         self.left = left
-         self.right = right
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> [int]:
-        lst = []
-        self.helper(root, lst)
-        return lst
-    def helper(self, root: TreeNode, lst: [int]):
-        if not root:
-            return
-        self.helper(root.left, lst)
-        lst.append(root.val)
-        self.helper(root.right, lst)
+        
+        # recursive
+        
+#         if not root: return
+        
+#         result = []
+#         self.helper(root, result)
+        
+#         return result
+    
+#     def helper(self, node: TreeNode, lst: List[int]):
+#         if not node: return
+        
+#         self.helper(node.left, lst)
+#         lst.append(node.val)
+#         self.helper(node.right, lst)
+        
+        
+        # iterative
+        
+        if not root: return
+        
+        result = []
+        stack = []
+        
+        curr = root
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+                
+            curr = stack.pop()
+            result.append(curr.val)
+            curr = curr.right
+            
+        return result
+        
+    
